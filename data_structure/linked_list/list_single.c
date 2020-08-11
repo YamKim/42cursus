55c69b9321cb83a136f9aa0ddc7c8081c7c6b512
yamkim <coramdeo0708@gmail.com>
2020-08-11 15:35:24 +0900

    [ds] add pop_back function in ft_lib.c

--- data_structure/linked_list/list_single.c
+++ data_structure/linked_list/list_single.c
@@ -34,13 +34,37 @@ int		push_back(t_node **head, void *data)
 	return (ret + 1);
 }
 
-int		get_size(t_node *head)
+#include <stdio.h>
+int		pop_back(t_node **head)
 {
 	int		ret;
+	t_node	*prev;
 	t_node	*cur;
+	
+	ret = 0;
+	cur = *head;
+	prev = cur;
+	while (cur)
+	{
+		if (!(cur->next))
+		{
+			prev->next = cur->next;
+			free(cur);
+			cur = NULL;
+			break ;
+		}
+		++ret;
+		prev = cur;
+		cur = cur->next;
+	}
+	return (ret);
+}
+
+int		get_size(t_node *cur)
+{
+	int		ret;
 
 	ret = 0;
-	cur = head;
 	while (cur)
 	{
 		++ret;
@@ -49,14 +73,29 @@ int		get_size(t_node *head)
 	return (ret);
 }
 
-int		get_data(t_node *head, int idx)
+void	*get_data(t_node *cur, int idx)
 {
-	t_node	*cur;
-
 	while (cur)
 	{
-		
+		if (!idx)
+			return (cur->data);	
 		cur = cur->next;
 		--idx;
 	}
+	return (NULL);
+}
+
+int		find_idx(t_node *cur, void *data_ref, int (*cmp)())
+{
+	int		ret;
+
+	ret = 0;
+	while (cur)	
+	{
+		if (cmp(cur->data, data_ref))
+			return (ret);
+		cur = cur->next;	
+		++ret;
+	}
+	return (-1);
 }
--- data_structure/linked_list/list_single.h
+++ data_structure/linked_list/list_single.h
@@ -5,7 +5,10 @@
 
 t_node	*create_node(void *data);
 int		push_back(t_node **head, void *data);
-int		get_size(t_node *head);
+int		pop_back(t_node **head);
+int		get_size(t_node *cur);
+void	*get_data(t_node *cur, int idx);
+int		find_idx(t_node *cur, void *data, int (*cmp)());
 
 #endif
 
