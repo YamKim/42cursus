fea2b5034a472467b36fe45f353d21aacaade35f
yamkim <coramdeo0708@gmail.com>
2020-08-12 17:04:05 +0900

    [ds] add functions in ft_lib.c

--- data_structure/linked_list/list_single.c
+++ data_structure/linked_list/list_single.c
@@ -11,14 +11,14 @@ t_node	*create_node(void *data)
 	return (ret);
 }
 
-int		push_back(t_node **head, void *data)
+int		push_back(t_node **lst, void *data)
 {
 	int		ret;
 	t_node	*cur;
 	t_node	*new_node;
 
 	ret = 0;
-	cur = *head;
+	cur = *lst;
 	while (cur)
 	{
 		++ret;	
@@ -34,15 +34,14 @@ int		push_back(t_node **head, void *data)
 	return (ret + 1);
 }
 
-#include <stdio.h>
-int		pop_back(t_node **head)
+int		pop_back(t_node **lst)
 {
 	int		ret;
 	t_node	*prev;
 	t_node	*cur;
 	
 	ret = 0;
-	cur = *head;
+	cur = *lst;
 	prev = cur;
 	while (cur)
 	{
@@ -99,3 +98,169 @@ int		find_idx(t_node *cur, void *data_ref, int (*cmp)())
 	}
 	return (-1);
 }
+
+int		ins_node(t_node **lst, void *data, int idx)
+{
+	t_node	*cur;
+	t_node	*new_node;
+
+	cur = *lst;
+	while (cur)
+	{
+		if (!(--idx))
+		{
+			new_node = create_node(data);
+			new_node->next = cur->next;
+			cur->next = new_node;
+			return (1);
+		}
+		cur = cur->next;
+	}
+
+	return (0);
+}
+
+int		del_node(t_node **lst, int idx)
+{
+	t_node	*prev;
+	t_node	*cur;
+
+	cur = *lst;
+	prev = cur;
+	while (cur)
+	{
+		if (!(idx--))
+		{
+			prev->next = cur->next;
+			free(cur);
+			cur = NULL;
+			return (1);
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+	return (0);
+}
+
+int		list_ins(t_node *lst, void *data, int idx)
+{
+	t_node	*cur;
+
+	cur = lst;
+	while (cur)
+	{
+		if (!(idx--))
+		{
+			cur->data = data; 
+			return (1);
+		}
+		cur = cur->next;
+	}
+	return (0);
+}
+
+void	list_clear(t_node *lst, enum TYPE t)
+{
+	t_node	*cur;
+
+	cur = lst;
+	while (cur)
+	{
+		if(t == TYPE_INT)
+			*(int *)(cur->data) = 0;
+		else if (t == TYPE_CHAR)
+			*(char *)(cur->data) = '\0';
+		else if (t == TYPE_STR)
+			cur->data = 0;
+		cur = cur->next;
+	}
+}
+
+void	list_free(t_node **lst)
+{
+	t_node	*cur;
+	t_node	*del_node;
+
+	cur = *lst;
+	while (cur)
+	{
+		del_node = cur;	
+		cur = cur->next;
+		free(del_node);
+		del_node = NULL;
+	}
+}
+
+/* linked list technic */
+t_node	*list_sort(t_node *cur, int (*cmp)())
+{
+	t_node	*ret;
+	t_node	*pos;
+	t_node	*min_node;
+	void	*tmp;
+
+	ret = cur;
+	while (cur)
+	{
+		min_node = cur;
+		pos = cur;
+		while (pos)
+		{
+			if(cmp(min_node->data, pos->data))
+				min_node = pos;
+			pos = pos->next;
+		}
+		tmp = cur->data;
+		cur->data = min_node->data;
+		min_node->data = tmp;
+		cur = cur->next;
+	}
+	return (ret);
+}
+
+void	list_remove_if(t_node **lst, void *data_ref, int (*cmp)())
+{
+	t_node	*tmp;
+	t_node	*prev;
+	t_node	*cur;
+
+	while (*lst && !cmp((*lst)->data, data_ref))
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		free(tmp);
+	}
+	prev = *lst;
+	while (prev && prev->next)
+	{
+		cur = prev->next;
+		if (!cmp(cur->data, data_ref))
+		{
+			tmp = cur;
+			prev->next = cur->next;
+			free(tmp);
+		}
+		else
+			prev = cur;
+	}
+}
+
+#include <stdio.h>
+void	list_print(t_node *lst, enum TYPE t)
+{
+	t_node	*cur;
+
+	cur = lst;
+	while (cur)
+	{
+		printf("addr of node: %p\n", cur);
+		if (t == TYPE_INT)
+			printf("data of node: %d\n", *(int *)(cur->data));
+		else if (t == TYPE_CHAR)
+			printf("data of node: %c\n", *(char *)(cur->data));
+		else if (t == TYPE_STR)
+			printf("data of node: %s\n", (char *)(cur->data));
+		cur = cur->next;
+	}
+}
+
--- data_structure/linked_list/list_single.h
+++ data_structure/linked_list/list_single.h
@@ -3,12 +3,31 @@
 
 # include "list.h"
 
+enum TYPE {
+	TYPE_CHAR,
+	TYPE_STR,
+	TYPE_INT,
+	TYPE_FLOAT
+};
+
 t_node	*create_node(void *data);
-int		push_back(t_node **head, void *data);
-int		pop_back(t_node **head);
+int		push_back(t_node **lst, void *data);
+int		pop_back(t_node **lst);
 int		get_size(t_node *cur);
 void	*get_data(t_node *cur, int idx);
 int		find_idx(t_node *cur, void *data, int (*cmp)());
+int		ins_node(t_node **lst, void *data, int idx);
+int		del_node(t_node **lst, int idx);
+int		list_ins(t_node *lst, void *data, int idx);
+void	list_clear(t_node *lst, enum TYPE t);
+void	list_free(t_node **lst);
+
+void	list_print(t_node *lst, enum TYPE t);
+
+t_node	*list_sort(t_node *cur, int (*cmp)());
+void	list_remove_if(t_node **lst, void *data_ref, int (*cmp)());
+
+
 
 #endif
 
