e3528cc9658ee36ecca8264404c50830bff3da21
yamkim <coramdeo0708@gmail.com>
2020-10-03 20:30:20 +0900

    [libft] upload libft functions to check norminette.

--- cursus/libft/ft_memccpy.c
+++ cursus/libft/ft_memccpy.c
@@ -6,13 +6,13 @@
 /*   By: yekim <yekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 16:06:03 by yekim             #+#    #+#             */
-/*   Updated: 2020/09/29 16:06:08 by yekim            ###   ########.fr       */
+/*   Updated: 2020/10/03 11:28:38 by yekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	size_t	idx;
 
--- cursus/libft/ft_memmove.c
+++ cursus/libft/ft_memmove.c
@@ -6,16 +6,28 @@
 /*   By: yekim <yekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 16:06:16 by yekim             #+#    #+#             */
-/*   Updated: 2020/10/01 06:11:32 by yekim            ###   ########.fr       */
+/*   Updated: 2020/10/03 20:23:11 by yekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	(void)dest;
-	(void)src;
-	(void)n;
+	int	idx;
+	int	len;
+
+	if (dest == src || n == 0)
+		return (dest);
+	len = (int)n;
+	idx = -1;
+	if (dest >= src)
+		while (++idx < len)
+			((unsigned char *)dest)[len - 1 - idx] 
+				= ((unsigned char *)src)[len - 1 - idx];
+	else
+		while (++idx < len)
+			((unsigned char *)dest)[idx] 
+				= ((unsigned char *)src)[idx];
 	return (dest);
 }
