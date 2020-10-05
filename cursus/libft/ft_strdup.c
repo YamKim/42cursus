9e1398cdccaeee8b4f1414b95f9e14e8b8aae8a7
yamkim <coramdeo0708@gmail.com>
2020-10-05 11:08:09 +0900

    [libft] modify files.

--- cursus/libft/ft_strdup.c
+++ cursus/libft/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yekim <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: yekim <yekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 16:01:38 by yekim             #+#    #+#             */
-/*   Updated: 2020/09/30 16:55:25 by yekim            ###   ########.fr       */
+/*   Updated: 2020/10/05 09:20:01 by yekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,11 @@
 char	*ft_strdup(const char *s)
 {
 	char	*ret;
-	int		idx;
 	int		len;
 
 	len = ft_strlen((char *)s);
 	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	ret[len] = '\0';
-	idx = 0;
-	while (s[idx] != '\0')
-	{
-		ret[idx] = s[idx];
-		++idx;
-	}
+	ft_strlcpy(ret, s, len + 1);
 	return (ret);
 }
