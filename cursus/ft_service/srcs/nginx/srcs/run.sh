31f97ae4f9b0be7df078d1e59a7661fcd667b524
yamkim <coramdeo0708@gmail.com>
2021-02-27 17:57:36 +0900

    [ft_service] restarting nginx from A to Z

--- cursus/ft_service/srcs/nginx/srcs/run.sh
+++ cursus/ft_service/srcs/nginx/srcs/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-mkdir -p /run/nginx
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-subj /C=KR/ST=Seoul/L=Seoul/O=42/OU=Lee/CN=nginxsvc \
+	-keyout /etc/ssl/nginx.key -out /etc/ssl/nginx.crt
 
-adduser -D ${SSH_USER}
-echo ${SSH_USER}:${SSH_PASSWD} | chpasswd
+adduser -D yekim
+echo "yekim:5933" | chpasswd
+
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ""
 
 /usr/sbin/sshd
-
 /usr/sbin/nginx -g "daemon off;"
